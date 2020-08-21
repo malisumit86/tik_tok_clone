e0d100446cc84929946a24f51ada672d3242c770
malisumit862 <sumitrm93@gmail.com>
2020-08-21 12:37:29 +0530

    Updates

---
+++ precache-manifest.1f951a7ca456b37ab833e2869ab063f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4c12347fcd03deec07cde10877f65ec",
+    "url": "/tik_tok_clone/index.html"
+  },
+  {
+    "revision": "5349cdbf0277d370f822",
+    "url": "/tik_tok_clone/static/css/main.6891abf2.chunk.css"
+  },
+  {
+    "revision": "30341b3c8849cbed1e9e",
+    "url": "/tik_tok_clone/static/js/2.3bd0cacd.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/tik_tok_clone/static/js/2.3bd0cacd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5349cdbf0277d370f822",
+    "url": "/tik_tok_clone/static/js/main.0a5ec7be.chunk.js"
+  },
+  {
+    "revision": "610df5f520e6f23cc2ca",
+    "url": "/tik_tok_clone/static/js/runtime-main.f99671ab.js"
+  }
+]);
